7666453ae88755b569b075d2b12f4061eb6b9924
Damien Dejean <dam.dejean@gmail.com>
2014-02-20 17:18:31 +0100

    Bootstrap: special location for bootstrap stack.
    Locate the boostrap stack in a special location to eae later memory mapping.
    Remove realmode setup and special section.

--- kernel/Boot/bootstrap.h
+++ kernel/Boot/bootstrap.h
@@ -18,5 +18,12 @@ extern void (*_ctors_end)(void);
 extern void (*_dtors_start)(void);
 extern void (*_dtors_end)(void);
 
+
+/**
+ * First stack size: 16 KB.
+ */
+#define BOOTSTRAP_STACK_SIZE       (16*1024)
+extern unsigned char boostrap_stack[];
+
 #endif
 
